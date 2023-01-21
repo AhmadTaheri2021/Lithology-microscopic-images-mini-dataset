075a162273c7bf60cc3ceef6667b3863643595d5
Ahmad Taheri <92728743+AhmadTaheri2021@users.noreply.github.com>
2023-01-21 14:27:41 +0330

    Update dataset_loader.py

--- dataset_loader.py
+++ dataset_loader.py
@@ -12,7 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 '''
-def Dataset_loader(path_):
+def Dataset_loader():
    
    def image_laoder_X():
     tr_images_path = {}
